8a0714d2fca0cec17d47c282273ea187a8c04a04
stlouisn <stlouisn@hotmail.com>
2019-04-19 10:31:39 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -2,22 +2,25 @@
 
 set -euo pipefail
 
-# Architectures to build
+# Platforms to build
 architectures="arm arm64 amd64"
 
-for arch in $architectures
-do
+for arch in $architectures; do
 
-	# Login into docker
-	echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
-
-	# Build temporary image
-	buildctl --debug build \
-		--frontend dockerfile.v0 \
-		--opt platform=linux/$arch \
-		--opt filename=docker/Dockerfile.${DOCKER_NAME}-${DOCKER_TAG}-$arch \
-		--local dockerfile=. \
-		--local context=. \
-		--output type=image,name=docker.io/${DOCKER_USERNAME}/${DOCKER_NAME}:${DOCKER_TAG}-$arch,push=true
+	docker build \
+	--platform=linux/$arch \
+    --label org.label-schema.build-date="$BUILD_DATE" \
+    --label org.label-schema.build-number="$BUILD_NUMBER" \
+    --label org.label-schema.description="$DOCKER_DESCRIPTION" \
+    --label org.label-schema.maintainer="$DOCKER_MAINTAINER" \
+    --label org.label-schema.name="$DOCKER_NAME" \
+    --label org.label-schema.url="$DOCKER_URL" \
+    --label org.label-schema.version="$DOCKER_VERSION" \
+    --label org.label-schema.schema-version="$SCHEMA_VERSION" \
+    --label org.label-schema.vcs-ref="$VCS_REF" \
+    --label org.label-schema.vcs-url="$VCS_URL" \
+	--tag $DOCKER_USERNAME/$DOCKER_NAME:$DOCKER_TAG-$arch \
+	--file docker/Dockerfile.$DOCKER_NAME-$DOCKER_TAG-$arch \
+	--pull
 
 done
