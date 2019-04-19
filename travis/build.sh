6a7a27f931307e197e4f0bea80d795ee48e2b96f
stlouisn <stlouisn@hotmail.com>
2019-04-19 14:10:44 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -7,7 +7,7 @@ architectures="arm arm64 amd64"
 
 for arch in $architectures; do
 
-	docker build \
+	DOCKER_BUILDKIT=1 docker build \
 	--platform linux/$arch \
 	--progress plain \
 	--tag "$DOCKER_USERNAME/$DOCKER_NAME:$DOCKER_TAG-$arch" \
