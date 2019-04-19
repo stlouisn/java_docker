94d376c7e4c3cc1d7799fa44a23fb3d04c86bb19
stlouisn <stlouisn@hotmail.com>
2019-04-19 13:00:53 -0300

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -8,19 +8,10 @@ architectures="arm arm64 amd64"
 for arch in $architectures; do
 
 	docker build \
-	--platform=linux/$arch \
-    	--label org.label-schema.build-date="$BUILD_DATE" \
-    	--label org.label-schema.build-number="$BUILD_NUMBER" \
-    	--label org.label-schema.description="$DOCKER_DESCRIPTION" \
-    	--label org.label-schema.maintainer="$DOCKER_MAINTAINER" \
-    	--label org.label-schema.name="$DOCKER_NAME" \
-    	--label org.label-schema.url="$DOCKER_URL" \
-    	--label org.label-schema.version="$DOCKER_VERSION" \
-    	--label org.label-schema.schema-version="$SCHEMA_VERSION" \
-    	--label org.label-schema.vcs-ref="$VCS_REF" \
-    	--label org.label-schema.vcs-url="$VCS_URL" \
-	--tag $DOCKER_USERNAME/$DOCKER_NAME:$DOCKER_TAG-$arch \
+	--platform linux/$arch \
+	--progress plain \
+	--tag "$DOCKER_USERNAME/$DOCKER_NAME:$DOCKER_TAG-$arch" \
 	--file docker/Dockerfile.$DOCKER_NAME-$DOCKER_TAG-$arch \
-	--pull
+	--pull docker/.
 
 done
