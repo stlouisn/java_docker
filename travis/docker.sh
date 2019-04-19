344cb64f3f4b33bdbfd124dd55755db7de4101ae
stlouisn <stlouisn@hotmail.com>
2019-04-19 14:03:01 -0300

    Create docker.sh

---
+++ travis/docker.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir -p /etc/docker
+
+cat <<'EOF' > /etc/docker/daemon.json
+{
+    "storage-driver": "overlay2",
+    "experimental":true
+}
+EOF
+
+cat /etc/docker/daemon.json
+
+cat <<'EOF' > /etc/apt/sources.list.d/docker.list
+deb [arch=armhf] https://download.docker.com/linux/debian stretch stable
+#deb [arch=armhf] https://download.docker.com/linux/debian stretch edge
+EOF
+
+#curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+# apt update -q
+
+#export DOCKER_BUILDKIT=1
+  
+ service docker restart || sudo journalctl -xe
+ 
+ docker info
+ docker version
